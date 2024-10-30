dfb962bf3c0454e0665791ebb02f1ced41120464
Jordan Nash <jordangabrielnash@gmail.com>
2024-10-30 12:08:38 -0700

    done with multiples

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ When called, your function should print out ALL of the multiples of `n` between 
 
 Call your function for `n = 2`, `n = 3`, `n = 5`, and `n = 12`.
 '''
+
+def print_multiples(n):
+    for i in range(101):
+        if (n*i)<=100:
+            print(n*i)
+
+print("multiples of 2")
+print_multiples(2)
+
+print("multiples of 3")
+print_multiples(3)
+
+print("multiples of 5")
+print_multiples(5)
+
+print("multiples of 12")
+print_multiples(12)
